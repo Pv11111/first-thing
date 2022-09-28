e937157e990888a63bb83ba5d261b39792bb2285
Pv11111 <114050507+Pv11111@users.noreply.github.com>
2022-09-28 09:58:14 +0300

    added scorboard

--- iduno.py
+++ iduno.py
@@ -1,22 +1,33 @@
 import random
 turns = ['rock', 'paper', 'scissors']
 
-while(True):
-    human_turn = input("Enter your turn, or type exit: ")
+human_turns = []
+computer_turns = []
+
+while True:
+    human_turn = input('Input human turn: ')
     computer_turn = random.choice(turns)
 
     if human_turn == 'exit':
-        print('Thank you for playing! Bye bye')
+        print('Thank you for playing!')
         break
+    
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
 
     print(f'Human:{human_turn} vs. Computer:{computer_turn}')
-    if human_turn == computer_turn:
-        print('Draw!')
-    elif human_turn == 'rock' and computer_turn == 'scissors':
-        print('Human wins!')
+
+    if human_turn == 'rock' and computer_turn == 'scissors':
+        print ('Human wins!')
     elif human_turn == 'paper' and computer_turn == 'rock':
-        print('Human wins!')
+        print ('Human wins!')
     elif human_turn == 'scissors' and computer_turn == 'paper':
-        print('Human wins!')
-    else:
-        print('Computer wins!')
+        print ('Human wins!')
+    if human_turn == computer_turn:
+        print('Draw!')  
+    else : print ('Computer wins!')
+
+print(f'You have played{len(human_turns)} times')
+print(human_turns)
+print(computer_turns)
